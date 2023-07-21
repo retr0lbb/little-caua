f16fe45395d11166ac09339b62b2f9b78b027f7b
Henrique Barbosa Sampaio <85702153+retr0lbb@users.noreply.github.com>
2023-07-20 22:38:40 -0300

    Add files via upload

---
+++ caua/script.js
@@ -0,0 +1,30 @@
+const radomPosition = () =>{
+    const screen = {
+        x: window.innerWidth,
+        y: window.innerHeight
+    }
+
+    var randomX = Math.random() *(screen.x -1000)
+    var randomY = Math.random() *(screen.y -1000)
+
+    return({x: randomX, y: randomY})
+}
+
+
+var button =document.querySelector("#butin")
+
+button.addEventListener("mouseover", () =>{
+    const randomPosition = radomPosition()
+    button.style.position = "absolute"
+    button.style.transform = `translate(${randomPosition.x}px, ${randomPosition.y}px)`
+    console.log(button.style)
+})
+
+
+console.log(button)
+
+var button2 = document.querySelector("#yes")
+
+button2.addEventListener("click", () =>{
+    alert("Ohhhhhh yeah")
+})
